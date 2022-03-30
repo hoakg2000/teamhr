cf8a5ef506e5a827fab1b9823431490a4e39577e
pduonghoa <hoakg2000@gmail.com>
2022-03-30 15:50:40 +0700

    update sql database

--- database/30032022.sql
+++ database/30032022.sql
@@ -26,8 +26,8 @@ CREATE TABLE `article` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `content` blob NOT NULL,
-  `create_at` datetime NOT NULL,
-  `update_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
   `team_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_article_team_idx` (`team_id`),
@@ -55,8 +55,8 @@ CREATE TABLE `checklist` (
   `id` int NOT NULL AUTO_INCREMENT,
   `team_id` int NOT NULL,
   `title` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `create_at` datetime NOT NULL,
-  `update_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_checklist_team_idx` (`team_id`),
   CONSTRAINT `fk_checklist_team` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`)
@@ -82,13 +82,11 @@ DROP TABLE IF EXISTS `column`;
 CREATE TABLE `column` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `type_id` int NOT NULL,
+  `type` enum('TXT','YN') COLLATE utf8mb4_unicode_ci NOT NULL,
   `checklist_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_Item_type_idx` (`type_id`),
   KEY `fk_column_checklist_idx` (`checklist_id`),
-  CONSTRAINT `fk_column_checklist` FOREIGN KEY (`checklist_id`) REFERENCES `checklist` (`id`),
-  CONSTRAINT `fk_column_type` FOREIGN KEY (`type_id`) REFERENCES `column_type` (`id`)
+  CONSTRAINT `fk_column_checklist` FOREIGN KEY (`checklist_id`) REFERENCES `checklist` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -99,29 +97,6 @@ CREATE TABLE `column` (
 LOCK TABLES `column` WRITE;
 /*!40000 ALTER TABLE `column` DISABLE KEYS */;
 /*!40000 ALTER TABLE `column` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `column_type`
---
-
-DROP TABLE IF EXISTS `column_type`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `column_type` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `type` enum('YN','CT','TXT') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'TXT',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `column_type`
---
-
-LOCK TABLES `column_type` WRITE;
-/*!40000 ALTER TABLE `column_type` DISABLE KEYS */;
-/*!40000 ALTER TABLE `column_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -174,29 +149,6 @@ CREATE TABLE `dev_skill` (
 LOCK TABLES `dev_skill` WRITE;
 /*!40000 ALTER TABLE `dev_skill` DISABLE KEYS */;
 /*!40000 ALTER TABLE `dev_skill` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `project`
---
-
-DROP TABLE IF EXISTS `project`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `project` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `project`
---
-
-LOCK TABLES `project` WRITE;
-/*!40000 ALTER TABLE `project` DISABLE KEYS */;
-/*!40000 ALTER TABLE `project` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -259,13 +211,9 @@ DROP TABLE IF EXISTS `team`;
 CREATE TABLE `team` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `project_id` int NOT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_team_project_idx` (`project_id`),
-  CONSTRAINT `fk_team_project` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -315,4 +263,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-30 11:59:46
+-- Dump completed on 2022-03-30 15:50:10
