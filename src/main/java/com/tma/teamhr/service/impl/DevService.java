456ebd9096ddc531f25b29da4385906c2df8b03a
saokykg <1851050051hoa@ou.edu.vn>
2022-04-04 23:32:13 +0700

    Autowrite devRepository in devService

--- src/main/java/com/tma/teamhr/service/impl/DevService.java
+++ src/main/java/com/tma/teamhr/service/impl/DevService.java
@@ -1,10 +1,16 @@
 package com.tma.teamhr.service.impl;
 
+import com.tma.teamhr.repository.IDevRepository;
 import com.tma.teamhr.service.IDevService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class DevService implements IDevService {
+
+    @Autowired
+    private IDevRepository devRepository;
+
 }
