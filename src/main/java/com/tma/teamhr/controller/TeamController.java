9c1b5aa2bc4c16f960137a931a76141a149123d7
saokykg <1851050051hoa@ou.edu.vn>
2022-04-05 14:55:40 +0700

    TM-8-team-api-create

--- src/main/java/com/tma/teamhr/controller/TeamController.java
+++ src/main/java/com/tma/teamhr/controller/TeamController.java
@@ -1,9 +1,19 @@
 package com.tma.teamhr.controller;
 
+import com.tma.teamhr.DTO.RequestDTO.TeamRequestDTO;
+import com.tma.teamhr.DTO.ResponseDTO.ResponseDTO;
+import com.tma.teamhr.DTO.ResponseDTO.TeamResponseDTO;
 import com.tma.teamhr.service.ITeamService;
+import com.tma.teamhr.utils.message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/teams")
@@ -12,4 +22,18 @@ public class TeamController {
     @Autowired
     private ITeamService teamService;
 
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDTO> create(@Valid @RequestBody TeamRequestDTO teamRequestDTO){
+        ResponseDTO responseDTO = new ResponseDTO();
+        try {
+            TeamResponseDTO data = teamService.create(teamRequestDTO);
+            responseDTO.setHeader(200);
+            responseDTO.setData(data);
+            responseDTO.setMessage(message.CREATE);
+        } catch (Exception ex){
+            responseDTO.setHeader(400);
+            responseDTO.setError(ex.getMessage());
+        }
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
 }
--- src/main/java/com/tma/teamhr/service/impl/TeamService.java
+++ src/main/java/com/tma/teamhr/service/impl/TeamService.java
@@ -1,5 +1,8 @@
 package com.tma.teamhr.service.impl;
 
+import com.tma.teamhr.DTO.RequestDTO.TeamRequestDTO;
+import com.tma.teamhr.DTO.ResponseDTO.TeamResponseDTO;
+import com.tma.teamhr.model.Team;
 import com.tma.teamhr.repository.ITeamRepository;
 import com.tma.teamhr.service.ITeamService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,4 +16,11 @@ public class TeamService implements ITeamService {
     @Autowired
     private ITeamRepository teamRepository;
 
+    @Override
+    public TeamResponseDTO create(TeamRequestDTO requestDTO){
+        Team dev = new Team();
+        dev.DTOtoEntity(requestDTO);
+        teamRepository.save(dev);
+        return new TeamResponseDTO(dev);
+    }
 }
