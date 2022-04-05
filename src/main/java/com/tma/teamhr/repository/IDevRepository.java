d183a2b1f6ff4da9c8519ad220330b40ddc97afe
saokykg <1851050051hoa@ou.edu.vn>
2022-04-05 19:19:59 +0700

    TM-25 reset project

--- src/main/java/com/tma/teamhr/repository/IDevRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tma.teamhr.repository;
-
-import com.tma.teamhr.model.Dev;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IDevRepository extends CrudRepository<Dev, Integer> {
-}
--- src/main/java/com/tma/teamhr/repository/IRowRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tma.teamhr.repository;
-
-import com.tma.teamhr.model.Row;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IRowRepository extends CrudRepository<Row, Integer> {
-}
