6b3b776a93cda5077f8abbed09502ccc874fb4ce
saokykg <1851050051hoa@ou.edu.vn>
2022-04-07 04:08:30 +0700

    [TM-21] implement api get team by id

--- src/main/java/com/tma/teamhr/controller/TeamController.java
+++ src/main/java/com/tma/teamhr/controller/TeamController.java
@@ -8,11 +8,11 @@ import com.tma.teamhr.utils.message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequestMapping("/api/v1/teams")
@@ -29,6 +29,20 @@ public class TeamController {
         skillList = teamService.getAll();
         responseDTO.setData(skillList);
         responseDTO.setMessage(message.GET);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}/get")
+    public ResponseEntity<ResponseDTO> getById(@PathVariable int id){
+        ResponseDTO responseDTO = new ResponseDTO();
+        responseDTO.setHeader(200);
+        try {
+            TeamResponseDTO teamResponseDTO = teamService.getById(id);
+            responseDTO.setData(teamResponseDTO);
+            responseDTO.setMessage(message.GET);
+        }catch (NullPointerException ex){
+            responseDTO.setError(ex.getMessage() + id);
+        }
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 }
