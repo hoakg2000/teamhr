e415e26d67ec9a8f8426b12b56e589e09b313199
saokykg <1851050051hoa@ou.edu.vn>
2022-04-08 11:11:09 +0700

    TM-38 implement api get checklist by id

--- src/main/java/com/tma/teamhr/service/impl/ChecklistServiceImpl.java
+++ src/main/java/com/tma/teamhr/service/impl/ChecklistServiceImpl.java
@@ -1,15 +1,18 @@
 package com.tma.teamhr.service.impl;
 
 import com.tma.teamhr.DTO.ResponseDTO.ChecklistResponseDTO;
+import com.tma.teamhr.ExceptionHandler.ApiRequestException;
 import com.tma.teamhr.model.Checklist;
 import com.tma.teamhr.repository.ChecklistRepository;
 import com.tma.teamhr.service.ChecklistService;
+import com.tma.teamhr.utils.message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -26,6 +29,15 @@ public class ChecklistServiceImpl implements ChecklistService {
             checkList.add(new ChecklistResponseDTO(check));
         });
         return checkList;
+    }
+
+    @Override
+    public ChecklistResponseDTO getById(int id) {
+        Optional<Checklist> optionalChecklist = checklistRepository.findById(id);
+        if (optionalChecklist.isEmpty())
+            throw new ApiRequestException(message.NOTEXIST_ID + id);
+
+        return new ChecklistResponseDTO(optionalChecklist.get());
     }
 
 
