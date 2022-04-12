3c0e119d176c99442cc35e01e29b1d8a4ba70c41
saokykg <1851050051hoa@ou.edu.vn>
2022-04-12 15:38:10 +0700

    extend ResponseEntityExceptionHandler

--- src/main/java/com/tma/teamhr/ExceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/tma/teamhr/ExceptionHandler/ApiExceptionHandler.java
@@ -5,12 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ApiRequestException.class)
     public ResponseEntity<ResponseDTO> handleApiRequestException(ApiRequestException e){
