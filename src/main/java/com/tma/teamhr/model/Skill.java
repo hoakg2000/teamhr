57df6487f35899d93baadf8352ccf7c8c67abc95
saokykg <1851050051hoa@ou.edu.vn>
2022-04-13 09:28:55 +0700

    TM-49

--- src/main/java/com/tma/teamhr/model/Skill.java
+++ src/main/java/com/tma/teamhr/model/Skill.java
@@ -1,10 +1,12 @@
 package com.tma.teamhr.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.tma.teamhr.DTO.RequestDTO.SkillRequestDTO;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
 @Table(name = "skill")
@@ -16,6 +18,10 @@ public class Skill {
     private int id;
 
     private String name;
+
+    @ManyToMany(mappedBy = "skills")
+    @JsonIgnore
+    private Collection<Dev> devs;
 
     public Skill(){}
 
